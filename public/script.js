c12a93b4ae0ccb3f53120a6da767f4c24930b4d7
Tripathi Sujal Prithvipal <146569069+SujalTripathi@users.noreply.github.com>
2025-06-07 17:44:45 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const APIURL = "http://localhost:3000/api/gemini"; 
+const APIURL = "/api/gemini";
 
 const chatArea = document.getElementById("chatArea");
 const input = document.getElementById("text-input");
